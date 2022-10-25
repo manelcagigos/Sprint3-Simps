a965688abb5f4056b16556b175f2d5696741e367
MC <manelcagigos@gmail.com>
2022-10-25 20:47:28 +0200

    frmTrustedUsers

--- MESSI/form_PaginaAdministracio.cs
+++ MESSI/form_PaginaAdministracio.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -20,6 +21,17 @@ namespace MESSI
         private void form_PaginaAdministracio_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
+        }
+
+        private void bt_GestioUsuaris_Click(object sender, EventArgs e)
+        {
+            Assembly asm = Assembly.GetEntryAssembly();
+            Type formtype = asm.GetType(string.Format("{0}.{1}", "Sprint3", "frmTrustedUsers"));
+
+            Form frmTrustedUsers = (Form)Activator.CreateInstance(formtype);
+            frmTrustedUsers.Show();
+
+            this.Hide();
         }
     }
 }
---
+++ MESSI/frmTrustedUsers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sprint3
+{
+    public partial class frmTrustedUsers : Form
+    {
+        public frmTrustedUsers()
+        {
+            InitializeComponent();
+        }
+
+        private void frmTrustedUsers_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
